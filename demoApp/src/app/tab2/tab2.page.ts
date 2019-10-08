a16ffd09e0e2cdd3b11e87bf6164d2f60d936070
Malick DIOP <diop-malick@hotmail.fr>
2019-10-08 15:24:32 +0200

    demo app - add polar custom connect method test

--- demoApp/src/app/tab2/tab2.page.ts
+++ demoApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Plugins} from '@capacitor/core';
+
+const {CapacitorPluginPolar} = Plugins;
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
 
-  constructor() {}
+    deviceId: any;
 
+    constructor() {
+
+        // Polar OH1 -> device id: 539B3229 -> address: A0:9E:1A:53:9B:32
+        this.deviceId = '539B3229';
+
+    }
+
+    async show() {
+        const ret = await CapacitorPluginPolar.echo({
+            value: 'Malick'
+        });
+        console.log('Got CapacitorPluginPolar.echo: ', JSON.stringify(ret));
+        console.log('Got CapacitorPluginPolar.echo.value: ', ret.value);
+    }
+
+    async connect() {
+        const ret = await CapacitorPluginPolar.connect({
+            value: this.deviceId
+        });
+    }
 }
